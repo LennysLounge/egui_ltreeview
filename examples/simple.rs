e4b39b9ee32b3842e671be4e81b70c5306cb0c1b
Leonard Schüngel <schuengel.leonard@gmail.com>
2024-02-09 22:22:55 +0100

    Simplify the simple example

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,9 +1,7 @@
 #[path = "data.rs"]
 mod data;
-use data::*;
 
-use egui_ltreeview::{TreeView, TreeViewBuilder};
-use uuid::Uuid;
+use egui_ltreeview::TreeView;
 
 fn main() -> Result<(), eframe::Error> {
     //env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
@@ -14,19 +12,17 @@ fn main() -> Result<(), eframe::Error> {
         ..Default::default()
     };
     eframe::run_native(
-        "Egui_ltreeview example",
+        "Egui_ltreeview simple example",
         options,
         Box::new(|_| Box::<MyApp>::default()),
     )
 }
 
-struct MyApp {
-    tree: Node,
-}
+struct MyApp {}
 
 impl Default for MyApp {
     fn default() -> Self {
-        Self { tree: make_tree() }
+        Self {}
     }
 }
 
@@ -34,31 +30,44 @@ impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             TreeView::new(ui.make_persistent_id("Names tree view")).show(ui, |mut builder| {
-                show_node(&mut builder, &self.tree);
+                builder.dir(0, |ui| {
+                    ui.add(egui::Label::new("Root").selectable(false));
+                });
+                builder.dir(1, |ui| {
+                    ui.add(egui::Label::new("Foo").selectable(false));
+                });
+                builder.leaf(2, |ui| {
+                    ui.add(egui::Label::new("Ava").selectable(false));
+                });
+                builder.dir(3, |ui| {
+                    ui.add(egui::Label::new("Bar").selectable(false));
+                });
+                builder.leaf(4, |ui| {
+                    ui.add(egui::Label::new("Benjamin").selectable(false));
+                });
+                builder.leaf(5, |ui| {
+                    ui.add(egui::Label::new("Charlotte").selectable(false));
+                });
+                builder.close_dir();
+                builder.close_dir();
+                builder.leaf(6, |ui| {
+                    ui.add(egui::Label::new("Daniel").selectable(false));
+                });
+                builder.leaf(7, |ui| {
+                    ui.add(egui::Label::new("Emma").selectable(false));
+                });
+                builder.dir(8, |ui| {
+                    ui.add(egui::Label::new("Baz").selectable(false));
+                });
+                builder.leaf(9, |ui| {
+                    ui.add(egui::Label::new("Finn").selectable(false));
+                });
+                builder.leaf(10, |ui| {
+                    ui.add(egui::Label::new("Grayson").selectable(false));
+                });
+                builder.close_dir();
+                builder.close_dir();
             });
         });
     }
-}
-
-fn show_node(builder: &mut TreeViewBuilder<Uuid>, node: &Node) {
-    match node {
-        Node::Directory(dir) => show_dir(builder, dir),
-        Node::File(file) => show_file(builder, file),
-    }
-}
-fn show_dir(builder: &mut TreeViewBuilder<Uuid>, dir: &Directory) {
-    builder.dir(dir.id, |ui| {
-        ui.label(&dir.name);
-    });
-
-    for node in dir.children.iter() {
-        show_node(builder, node);
-    }
-
-    builder.close_dir();
-}
-fn show_file(builder: &mut TreeViewBuilder<Uuid>, file: &File) {
-    builder.leaf(file.id, |ui| {
-        ui.label(&file.name);
-    });
 }
