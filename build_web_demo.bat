5f669df67a589759d971e120745f881517d29616
Leonard Schüngel <schuengel.leonard@gmail.com>
2025-10-06 21:59:12 +0200

    Adjust workflow to build and deploy demo

--- build_web_demo.bat
+++ build_web_demo.bat
@@ -1,2 +1,2 @@
 cargo build --example playground --release --target wasm32-unknown-unknown
-wasm-bindgen ./target/wasm32-unknown-unknown/release/examples/playground.wasm --no-modules --no-typescript --out-dir web_demo
+wasm-bindgen ./target/wasm32-unknown-unknown/release/examples/playground.wasm --target web --out-dir web_demo --no-typescript
